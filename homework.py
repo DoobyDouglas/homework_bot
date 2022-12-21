060ab02cf2c34593a318e42981943e812d3adcef
Антон Уваров <doobydouglas90@gmail.com>
2022-12-21 03:57:14 +0300

    BotHomeWorkFix

--- homework.py
+++ homework.py
@@ -6,6 +6,7 @@ import requests
 import time
 
 from dotenv import load_dotenv
+from json.decoder import JSONDecodeError
 
 
 load_dotenv()
@@ -35,6 +36,12 @@ formatter = logging.Formatter(
 )
 handler.setFormatter(formatter)
 
+type_error = 'Ошибка типа данных'
+api_key_error = 'Ключа нет в ответе от API'
+no_status = 'Отсутствие статуса'
+strange_status = 'Неожиданный статус домашней работы'
+main_error = 'Сбой в работе программы: '
+
 
 def check_tokens():
     """Проверяет наличие токенов."""
@@ -48,8 +55,6 @@ def check_tokens():
         if value is None:
             logger.critical(f'{key} отсутствует')
             raise SystemExit
-        else:
-            return True
 
 
 def send_message(bot, message):
@@ -57,8 +62,9 @@ def send_message(bot, message):
     try:
         bot.send_message(TELEGRAM_CHAT_ID, message)
         logger.debug('Сообщение отправлено')
-    except Exception:
-        logger.error('Ошибка при отправке сообщения')
+    except Exception as error:
+        logger.error(f'Ошибка при отправке сообщения: {error}')
+        print(logger.error)
 
 
 def get_api_answer(timestamp):
@@ -77,20 +83,19 @@ def get_api_answer(timestamp):
 
     try:
         response = homework_statuses.json()
-    except Exception:
-        logger.error('Ответ не JSON')
+    except JSONDecodeError:
+        logger.error(type_error)
     return response
 
 
 def check_response(response):
     """Проверяет ответ от API."""
-    if type(response) is dict:
-        if 'homeworks' in response:
-            if not isinstance(response['homeworks'], list):
-                raise TypeError('Ошибка типа')
-        else:
-            logger.error('Ключа нет в ответе от API')
-            raise Exception('Ключа нет в ответе от API')
+    if isinstance(response, dict):
+        if 'homeworks' not in response:
+            logger.error(api_key_error)
+            raise KeyError(api_key_error)
+        if not isinstance(response['homeworks'], list):
+            raise TypeError(type_error)
     homework = response['homeworks']
     return homework
 
@@ -98,18 +103,18 @@ def check_response(response):
 def parse_status(homework):
     """Проверяет статус домашней работы."""
     if 'homework_name' not in homework:
-        logger.error('Ключа нет в ответе от API')
-        raise Exception('Ключа нет в ответе от API')
+        logger.error(api_key_error)
+        raise KeyError(api_key_error)
     homework_name = homework['homework_name']
     status = homework['status']
     if status in HOMEWORK_VERDICTS:
         verdict = HOMEWORK_VERDICTS[status]
     elif status is None:
-        logger.error('Отсутствие статуса')
-        raise Exception('Отсутствие статуса')
+        logger.error(no_status)
+        raise Exception(no_status)
     else:
-        logger.error('Неожиданный статус домашней работы')
-        raise Exception('Неожиданный статус домашней работы')
+        logger.error(strange_status)
+        raise Exception(strange_status)
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
@@ -129,8 +134,8 @@ def main():
                 send_message(bot, message)
             time.sleep(RETRY_PERIOD)
         except Exception as error:
-            message = f'Сбой в работе программы: {error}'
-            logger.error(f'Сбой в работе программы: {error}')
+            message = f'{main_error}{error}'
+            logger.error(f'{main_error}{error}')
             time.sleep(RETRY_PERIOD)
 
 
